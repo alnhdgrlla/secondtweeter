151e9f9f92e84023be3120a3549ee63cee97b056
alnhdgrlla <y.tanaka@shippio.io>
2019-10-09 12:03:46 +0900

    user update

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "tweets#index"
 
-  resources :users, only: [:index, :show, :edit, :create]
+  resources :users, only: [:index, :show, :edit, :update, :create]
   resources :tweets, only: [:index, :create, :destroy]
 end
