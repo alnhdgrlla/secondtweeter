de729a00ea07bd0e99d1477780be24f0c9f75a09
alnhdgrlla <y.tanaka@shippio.io>
2019-10-17 15:53:17 +0900

    added validation to edit user

--- app/graphql/mutations/edit_user.rb
+++ app/graphql/mutations/edit_user.rb
@@ -1,23 +1,21 @@
 module Mutations
   class EditUser < BaseMutation
 
+    argument :user_id, String, required: true
     argument :username, String, required: true
     argument :bio, String, required: true
     argument :password, String, required: true
 
     type Types::UserType
     
-    def resolve(username:, bio:, password:)
-      user = context[:current_user]
-      if user.update!(username: username, bio: bio, password: password)
+    def resolve(user_id:, username:, bio:, password:)
+      user = User.find(user_id)
+      user.update!(username: username, bio: bio, password: password) if user == context[:current_user]
         {
           username: username,
           bio: bio,
           password: password
         }
-      else
-        put "Following did not finish successfuly. Please try again"
-      end
     end
   end
 end
